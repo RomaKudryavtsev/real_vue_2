0c77001f4776389010bb8ca9f0c1d3b0a1977a24
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2024-08-16 23:39:09 +0300

    add progress bar using axios interceptors

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 // import BaseIcon from "./components/BaseIcon.vue";
 import upperFirst from "lodash/upperFirst";
 import camelCase from "lodash/camelCase";
+import "nprogress/nprogress.css";
 
 const requireComponent = require.context(
   "./components",
